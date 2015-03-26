8d15911af4c092a62909b2a632db511e1d042296
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-26 22:52:02 +0100

    future support

---
+++ common/src/main/scala/nl/proja/pishake/common/FutureSupport.scala
@@ -0,0 +1,28 @@
+package nl.proja.pishake.common
+
+import akka.util.Timeout
+
+import scala.concurrent._
+import scala.util.{Failure, Success}
+
+trait FutureSupport {
+
+  def offLoad(future: Future[Any])(implicit timeout: Timeout): Any = {
+    Await.ready(future, timeout.duration)
+    future.value.get match {
+      case Success(result) => result
+      case Failure(result) => result
+    }
+  }
+
+  def sequentialExecution(futures: Seq[Future[Any]])(implicit ec: ExecutionContext): Future[List[Any]] = {
+    futures.foldLeft(Future(List.empty[Any])) {
+      (previousFuture, next) =>
+        for {
+          previousResults <- previousFuture
+          next <- next
+        } yield previousResults :+ next
+    }
+  }
+}
+
