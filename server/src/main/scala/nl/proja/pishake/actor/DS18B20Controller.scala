d7265137f0c2d9713379f5fce4b3418fff855cd1
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-04-05 23:30:25 +0200

    doesn't fail if sensor is not d218b20

--- server/src/main/scala/nl/proja/pishake/actor/DS18B20Controller.scala
+++ server/src/main/scala/nl/proja/pishake/actor/DS18B20Controller.scala
@@ -1,11 +1,12 @@
 package nl.proja.pishake.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
-import nl.proja.pishake.operation.DS18B20Controller.{Temperature, ReadTemperature}
+import nl.proja.pishake.operation.DS18B20Controller.{ReadTemperature, Temperature}
 import nl.proja.pishake.util.{ActorDescription, ActorSupport}
 
 import scala.language.postfixOps
 import scala.sys.process._
+import scala.util.{Success, Try}
 
 object DS18B20Controller extends ActorDescription {
 
@@ -24,15 +25,17 @@ class DS18B20Controller extends Actor with ActorLogging with ActorSupport {
 
   def readDS18B20: List[Temperature] = {
     val path = "/sys/bus/w1/devices"
-
     Seq("ls", path).lineStream.filter(sensor.findFirstIn(_).isDefined).flatMap { sensor =>
-      Seq("cat", s"$path/$sensor/w1_slave").lineStream.toList match {
+      Try(Seq("cat", s"$path/$sensor/w1_slave").lineStream.toList match {
         case head :: tail :: Nil => dS18B20FirstLine.findFirstIn(head).flatMap {
           _ => tail match {
             case dS18B20SecondLine(temperature) => Some(Temperature(sensor, temperature.toDouble / 1000) :: Nil)
             case _ => None
           }
         } getOrElse Nil
+        case _ => Nil
+      }) match {
+        case Success(result) => result
         case _ => Nil
       }
     } toList
